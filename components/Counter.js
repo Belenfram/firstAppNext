1fe626cfb3d40a17167aed5499301981af17cbd9
Belenfram <huchimonofrey.b@gmail.com>
2024-03-04 15:53:00 -0600

    useState

--- components/Counter.js
+++ components/Counter.js
@@ -1,23 +1,4 @@
 'use client'
-/*import React, { useState } from 'react';
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <h2>Contador</h2>
-      <p>El valor actual es: {count}</p>
-      <button onClick={increment}>Incrementar</button>
-    </div>
-  );
-};
-
-export default Counter;*/
 
 import React, { useState } from 'react';
 
